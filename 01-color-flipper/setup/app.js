53eacdbb2f46a5cb94b67e23ec748826876050e8
Mustansir Rangwala <119410651+mustansirr@users.noreply.github.com>
2025-01-23 23:51:24 +0530

    feat: color switching

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const background = document.querySelector('body');
+const button = document.getElementById('btn');
+const hexText = document.querySelector('.color');
+button.style.backgroundColor = "white";
+button.style.color = "black";
+button.addEventListener('click', (e) => {
+    const randomColor = Math.floor(Math.random() * colors.length);
+    background.style.backgroundColor = colors[randomColor];
+    hexText.textContent = colors[randomColor];
+})  
