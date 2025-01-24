f4ff33bd72fcab2ff712bceb9227b97cbe64e28d
Mustansir Rangwala <119410651+mustansirr@users.noreply.github.com>
2025-01-24 11:06:31 +0530

    feat: switch between hex colors

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    let randomNumber;
+    let hexColor = "#";
+    for(let i = 0; i < 6;i++) {
+    randomNumber = Math.floor(Math.random() * hex.length);
+    hexColor += hex[randomNumber];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
